94b7fd0719b0c594ee4f7413166054eecb7f28cd
Joe Karayusuf <jkarayusuf@gmail.com>
2013-05-28 21:36:34 -0700

    Vundle works

--- install.sh
+++ install.sh
@@ -4,4 +4,10 @@ set -e
 # Full path to local repository
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+rm -rf ~/.gitconfig
+rm -rf ~/.vimrc
+rm -rf ~/.vim
+
 ln -s "${DIR}/gitconfig" ~/.gitconfig
+ln -s "${DIR}/vim/vimrc" ~/.vimrc
+ln -s "${DIR}/vim" ~/.vim
